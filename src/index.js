d916e7ecb5e5daaea3feed7b776b14146bf0424e
Nicholas Braun <nicholas.braun@ressourcenmangel.de>
2019-02-25 12:09:20 +0100

    Add syntax highlighting

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import addons from '@storybook/addons';
 import pretty from 'pretty';
+import Prism from 'prismjs';
 
 import ReactDOMServer from 'react-dom/server'
 
@@ -8,7 +9,7 @@ export class ShowStaticMarkup extends React.Component {
   render() {
     const { children } = this.props;
 
-    const markup = pretty(ReactDOMServer.renderToStaticMarkup(children));
+    const markup = Prism.highlight(pretty(ReactDOMServer.renderToStaticMarkup(children)));
 
     const channel = addons.getChannel();
     channel.emit('evgenykochetkov/static-markup/show-markup', markup);
